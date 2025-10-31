b3d9211a733e8045fe17907f4449d0859651df99
botcheez <62220452+botcheez@users.noreply.github.com>
2025-10-30 22:36:25 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>;
 
 int main() {
-    printf("Hello teach. My name is <your name here> and I'm in your CMPE310 class.");
+    printf("Hello teach. My name is William DuBois and I'm in your CMPE310 class.");
     return 0;
 }
